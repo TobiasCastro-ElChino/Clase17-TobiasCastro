cdc08e89644d9f3a0c457e226ea4f6c9e3821876
TobiasCastro-ElChino <tobiasjch13@gmail.com>
2023-02-06 16:33:28 -0300

    Finalizando Header

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath))
 
 app.get('/', (req,res) => res.sendFile(path.resolve('./views/home.html')))
-
+app.get('/register', (req,res) => res.sendFile(path.resolve('./views/register.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve('./views/login.html')))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
